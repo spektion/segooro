cb60ebe64e9613c4b4fe4564337b7dce25b594be
Luixpt <franco.luis@gmail.com>
2015-01-17 16:35:07 +0000

    Merge origin/master
    Conflicts:
    	src/segooro/MainWindow.java

--- src/segooro/Tssaude.java
+++ src/segooro/Tssaude.java
@@ -6,7 +6,7 @@
 package segooro;
 
 import java.io.Serializable;
-
+import Excepcoes.*;
 /**
  *
  * @author a
@@ -18,6 +18,27 @@ public class Tssaude extends Seguro  implements Serializable{  //Declaração da
     public Tssaude(float p, int m){  //Construtor
         super(p,tseguro.Saude);  //Usa o metodo da superclass
         this.maximo_a_reembolsar=m;  //Atribui o maximo
+    }
+    
+    public void tssaudeisvalid(int idd, int tseguro) throws Exception {
+        int idadeaux=0;
+        
+        if (idadeaux>80){ //Se tiver acima de 80 não pode fazer seguro
+           throw new SinvalidoException("Idade do utilizador ja nao permite realizar seguro");
+        }
+        else
+           if ((idadeaux<17) &&(tseguro!=0) ){  //Se tiver menos de 17 é junior
+               throw new SjuniorException("Idade do utilizador(<17) nao e compativel com o tipo de seguro escolhido");
+           }
+           else{
+               if ((idadeaux>59)&&(tseguro!=2))  //É senior?
+                 throw new SseniorException("Idade do utilizador(>59) nao e compativel com o tipo de seguro escolhido");
+           
+               else{ //Então é adulto
+                   if (tseguro!=1)
+                    throw new SadultoException("Idade do utilizador(>17 e <59) nao e compativel com o tipo de seguro escolhido");
+               }
+           }
     }
     
    @Override
